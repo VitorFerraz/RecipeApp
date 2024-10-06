e808c397922e9e6158dbbeb05e08fe9ff77aa1df
Vitor Ferraz Varela <vitorfvarela@gmail.com>
2024-10-06 12:12:12 -0300

    Initial Commit

---
+++ RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by Vitor Ferraz Varela on 06/10/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
